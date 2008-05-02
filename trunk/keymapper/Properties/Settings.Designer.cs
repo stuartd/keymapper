d60ad2dbf3ca755dfa10786062f2695fb24ad8b8
stuartd <stuartd@49a0e6fb-0f4b-0410-acf7-fff891c7cd64>
2008-05-02 01:29:48 +0000

    Added upgrade to settings, changed Publish settings.

--- trunk/keymapper/Properties/Settings.Designer.cs
+++ trunk/keymapper/Properties/Settings.Designer.cs
@@ -226,5 +226,17 @@ namespace KeyMapper.Properties {
                 this["ColourMapShowAllButtons"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UpgradeRequired {
+            get {
+                return ((bool)(this["UpgradeRequired"]));
+            }
+            set {
+                this["UpgradeRequired"] = value;
+            }
+        }
     }
 }
