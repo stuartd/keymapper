a61326e14414538e6b6dd717f949d5487b9cabc8
stuartd <stuartd@49a0e6fb-0f4b-0410-acf7-fff891c7cd64>
2008-06-04 00:03:57 +0000

    Removed some files

--- trunk/website/KMBlog/KMBlog.Master.designer.cs
+++ trunk/website/KMBlog/KMBlog.Master.designer.cs
@@ -14,6 +14,15 @@ namespace KMBlog {
     public partial class KMBlog {
         
         /// <summary>
+        /// kmhead control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlHead kmhead;
+        
+        /// <summary>
         /// head control.
         /// </summary>
         /// <remarks>
@@ -23,12 +32,12 @@ namespace KMBlog {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// body control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder body;
     }
 }
