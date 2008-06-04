1b5b57dcbb50a8f7c872e1a2c7e24f14cf6881ed
stuartd <stuartd@49a0e6fb-0f4b-0410-acf7-fff891c7cd64>
2008-06-04 15:03:38 +0000

    Website: updated.

--- trunk/website/KMBlog/Default.aspx.designer.cs
+++ trunk/website/KMBlog/Default.aspx.designer.cs
@@ -21,5 +21,23 @@ namespace KMBlog {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// postsRepeater control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater postsRepeater;
+        
+        /// <summary>
+        /// SQLBlogData control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SQLBlogData;
     }
 }
