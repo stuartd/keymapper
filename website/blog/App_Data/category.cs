08c25fc5a3bda494c137e8d24e342a08075851a6
stuartd <stuartd@49a0e6fb-0f4b-0410-acf7-fff891c7cd64>
2008-07-01 23:20:10 +0000

    Now things work, time to decouple. Added classes for blog, post and category.

---
+++ website/blog/App_Data/category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KMBlog
+{
+    public class Category
+    {
+        public int ID { get; private set; }
+        public string Name { get; set; }
+
+        public Category CreateNewCategory()
+        {
+            return new Category();
+        }
+
+        private Category()
+        {
+            ID = 0;
+            Name = String.Empty;
+        }
+    }
+}
---
+++ website/blog/App_Data/post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KMBlog
+{
+    public class post
+    {
+
+        public int ID { get; private set; }
+        public string Title { get; set; }
+        public List<Category> Categories { get; private set; }
+        public DateTime Postdate { get; set; }
+
+        public post CreateNewPost()
+        {
+            Title = String.Empty;
+            Postdate = DateTime.Now;
+            Categories = new List<Category>();
+        }
+
+        public void AddCategoryToPost() { }
+
+        public void DeleteCategoryFromPost() { }
+
+        public void DeletePost(int id) { }
+
+
+    }
+}
