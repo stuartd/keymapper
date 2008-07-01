25cb2fb34f543672f2f468e22fd6a278847e1b24
stuartd <stuartd@49a0e6fb-0f4b-0410-acf7-fff891c7cd64>
2008-07-01 23:20:10 +0000

    Now things work, time to decouple. Added classes for blog, post and category.

---
+++ trunk/website/blog/App_Data/blog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic ;
+
+namespace KMBlog
+{
+
+    public class blog
+    {
+
+        public string Title { get; set; }
+        // private List<post> _posts;
+
+        public post GetPostByID(int id)
+        {
+            // DataProvider dp = new dataProvider();
+            // return dp.GetPostByID(id);
+
+            
+        }
+    }
+}
