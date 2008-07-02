358931c2b7c7883319b0161dd8a20b8cca4704ae
stuartd <stuartd@49a0e6fb-0f4b-0410-acf7-fff891c7cd64>
2008-07-02 15:14:21 +0000

    KMBlog: Added interface file to SVN

---
+++ trunk/website/blog/Code/IBlogData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KMBlog
+{
+	internal interface IBlogData
+	{
+
+		Post GetPostByID(int postID);
+
+		Post AddPost(Post p);
+
+		Post EditPost(Post p);
+
+		bool AddCategoryToPost(int postID, Category cat);
+
+		bool DeleteCategoryFromPost(int postID, Category cat);
+
+		bool DeletePost(int id);
+
+	}
+}
