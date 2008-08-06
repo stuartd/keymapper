0832ab365a2a804f45d19912b121c6e2627847ef
stuartd <stuartd@49a0e6fb-0f4b-0410-acf7-fff891c7cd64>
2008-08-06 15:04:06 +0000

    KMBlog: implemented JavaScript confirm for deletions

--- website/blog/admin/edit-categories.aspx.cs
+++ website/blog/admin/edit-categories.aspx.cs
@@ -16,10 +16,13 @@ namespace KMBlog
 
             newcategory.CategorySaved += NewCategorySaved;
 
-			ClientScript.RegisterClientScriptBlock(this.GetType(),
+			if (ClientScript.IsClientScriptBlockRegistered("confirmButtonScript") == false)
+			{
+				ClientScript.RegisterClientScriptBlock(this.GetType(),
 			"confirmButtonScript",
-			"function __doConfirm(btn){if (confirm('Are you sure you wish to submit?')){return true;}else{return false;}}",
+			"function __doConfirm(btn){if (confirm('Are you sure you wish to delete this category?')){return true;}else{return false;}}",
 			true);
+			}
 
         }
 
