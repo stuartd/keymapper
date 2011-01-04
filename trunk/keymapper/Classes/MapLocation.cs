ca7990bc5abf970d8adbac16115690cff2c0685a
stuartd <stuartd@49a0e6fb-0f4b-0410-acf7-fff891c7cd64>
2011-01-04 23:14:29 +0000

    Gone a bit tits-i[ on the svn front here..

---
+++ trunk/keymapper/Classes/MapLocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyMapper.Classes
+{
+    public enum MapLocation
+    {
+        LocalMachineKeyboardLayout,
+        CurrentUserKeyboardLayout,
+        KeyMapperLocalMachineKeyboardLayout,
+        KeyMapperCurrentUserKeyboardLayout,
+        KeyMapperVistaMappingsCache
+    }
+}
---
+++ trunk/keymapper/Classes/Mappings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyMapper.Classes
+{
+    public enum Mappings
+    {
+        CurrentBootMappings, CurrentUserMappings, SavedBootMappings, SavedUserMappings
+    }
+}
